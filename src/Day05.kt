fb8c6c851e850a57770c0fe92f6e6486142d288a
pmellaaho <pmellaaho@gmail.com>
2024-12-27 19:35:02 +0200

    Day05 Pt.II

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,68 +1,93 @@
+import java.time.zone.ZoneRules
+
+data class Rule(val precedingPage: Int, val latterPage: Int)
+
+lateinit var pageOrderingRules: Set<Rule>
+lateinit var updates: List<List<Int>>
+
 fun main() {
 
-    data class Rule(val precedingPage: Int, val latterPage: Int)
-
-    fun part1(input: List<String>): Int {
+    fun initData(input: List<String>) {
         val (a, b) = input.partition { it.contains('|') }
 
         val regex = "\\D+".toRegex()
-        val pageOrderingRules = a.map { line ->
+        pageOrderingRules = a.map { line ->
             line.split(regex).let {
                 Rule(it.first().toInt(), it.last().toInt())
             }
-        }
-        "There are ${pageOrderingRules.size} rules alltogether".println()
+        }.toSet()
+//        "There are ${pageOrderingRules.size} rules alltogether".println()
 
-        val updates = b.drop(1).map {
+        updates = b.drop(1).map {
             it.split(',').map { s -> s.toInt() }
         }
+    }
 
-        val inCorrectOrder =
-        updates.filter { update ->
-            val rulesThatApply = pageOrderingRules.filter { rule ->
-                update.contains(rule.precedingPage) && update.contains(rule.latterPage)
+    fun List<Int>.rulesThatApply(): List<Rule> = pageOrderingRules.filter { rule ->
+        this.contains(rule.precedingPage) && this.contains(rule.latterPage)
+    }
+
+    val isUpdateInCorrectOrder: (List<Int>) -> Boolean = { update ->
+        val rulesForUpdate = update.rulesThatApply()
+        var correctOrder = true
+        update.forEachIndexed { index, page ->
+            val rulesForPage = rulesForUpdate.filter {
+                page == it.precedingPage || page == it.latterPage
             }
-//            "For update: $update, there are ${rulesThatApply.size} rules that apply".println()
-            var correctOrder = true
 
-            update.forEachIndexed { index, page ->
-                val rulesForPage = rulesThatApply.filter {
-                    page == it.precedingPage || page == it.latterPage }
-
-//                "For page: $page, there are ${rulesForPage.size} rules that apply".println()
-                for(rule in rulesForPage) {
-                    if(rule.precedingPage == page) {
-                        if (index > update.indexOf(rule.latterPage)) {
-                            correctOrder = false
-                            return@forEachIndexed
-                        }
-                    } else {
-                        if (index < update.indexOf(rule.precedingPage)) {
-                            correctOrder = false
-                            return@forEachIndexed
-                        }
+            for (rule in rulesForPage) {
+                if (rule.precedingPage == page) {
+                    if (index > update.indexOf(rule.latterPage)) {
+                        correctOrder = false
+                        return@forEachIndexed
+                    }
+                } else {
+                    if (index < update.indexOf(rule.precedingPage)) {
+                        correctOrder = false
+                        return@forEachIndexed
                     }
                 }
             }
-            correctOrder
         }
+        correctOrder
+    }
 
-        return inCorrectOrder.sumOf { it.get(it.lastIndex / 2) }
+    fun part1(input: List<String>): Int {
+        initData(input)
+        val updatesWithCorrectOrder = updates.filter { isUpdateInCorrectOrder(it) }
+        return updatesWithCorrectOrder.sumOf { it.get(it.lastIndex / 2) }
+    }
 
+    class RuleComparator(private val rules: List<Rule>) : Comparator<Int> {
+        override fun compare(x: Int?, y: Int?): Int =
+            when {
+                rules.any { it.precedingPage == x && it.latterPage == y } -> 1
+                rules.any { it.precedingPage == y && it.latterPage == x } -> -1
+                else -> 0
+            }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        initData(input)
+        val updatesWithIncorrectOrder = updates.filterNot { isUpdateInCorrectOrder(it) }
+
+        val fixedUpdates = updatesWithIncorrectOrder.map { update ->
+            val rulesForUpdate = update.rulesThatApply()
+            val comparator = RuleComparator(rulesForUpdate)
+            update.sortedWith(comparator)
+        }
+        return fixedUpdates.sumOf { it.get(it.lastIndex / 2) }
     }
 
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 143)
-//    check(part2(testInput) == 31)
+    check(part2(testInput) == 123)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day05")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
+//    check(part2(input) == 5180)
 }
